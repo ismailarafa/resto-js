b6198cf54c99b04eaca660a3ee3069e445470a45
Ismail Arafa <ismailarafa18@gmail.com>
2017-06-05 12:52:43 -0400

    finish

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var model = {
+var view = {
   createMainDiv: function () {
     var div = document.createElement('div');
     var bodyElement = document.querySelector('body');
@@ -26,39 +26,16 @@ var model = {
     secondHeader.innerHTML = text;
     return secondHeader;
   },
-  createNavBtn: function (right) {
+  createNavBtn: function (right, id) {
     var navBtn = document.createElement('i');
     if (right) {
       navBtn.className = 'fa fa-arrow-right fa-lg fa-2x';
     } else {
       navBtn.className = 'fa fa-arrow-left fa-lg fa-2x';
     }
+    navBtn.id = id;
     return navBtn;
-  }
-};
-
-var handlers = {
-  createMenuPage: function () {
-    var mainDiv = document.querySelector('div');
-    mainDiv.innerHTML = '';
-    mainDiv.appendChild(model.createHeader('Mouthwatering Meat Treats'));
-    mainDiv.appendChild(model.createHeroImage('images/kabab.jpeg'));
-    mainDiv.appendChild(model.createSecondHeader("Kababatory's Infamous Menu"));
-    mainDiv.appendChild(model.createText('Stay tuned for the menu or give us a call at 1-800-KABAB to find out about the freshest daily catch.'));
-    mainDiv.appendChild(model.createNavBtn(true));
-    mainDiv.appendChild(model.createNavBtn(false));
   },
-  createAboutPage: function () {
-    var mainDiv = document.querySelector('div');
-    mainDiv.innerHTML = '';
-    mainDiv.appendChild(model.createHeader('Kababatory Origins'));
-    mainDiv.appendChild(model.createHeroImage('images/prince.jpg'));
-    mainDiv.appendChild(model.createSecondHeader("A Prince's Journey"));
-    mainDiv.appendChild(model.createText("The story of the prince is one of persistence, hard work and real dedication to one's craft. Starting off on a cart, he slowly built his empire through his infamous liver sandwiches and quickly became a staple in street food, subsequently expanding into a full-blown restaurant where he can fulfill his passion of serving irresistible meats."));
-    mainDiv.appendChild(model.createNavBtn(false));
-  }
-};
-var view = {
   createHomePage: function () {
     var bodyElement = document.querySelector('body');
     var mainDiv = document.querySelector('div');
@@ -66,27 +43,47 @@ var view = {
     mainDiv.innerHTML = '';
     bodyElement.insertBefore(mainDiv, bodyElement.firstChild);
     mainDiv.innerHTML = '';
-    mainDiv.appendChild(model.createHeader('Welcome to the Kababatory!'));
-    mainDiv.appendChild(model.createHeroImage('images/fire.jpg'));
-    mainDiv.appendChild(model.createSecondHeader('Our Food'));
-    mainDiv.appendChild(model.createText('We offer the most premium of halal beef, perfectly prepared into the most succulent of meat cutlets before getting smoked for hours on end in our very own charcoal grill.'));
-    mainDiv.appendChild(model.createNavBtn(true));
+    mainDiv.appendChild(view.createHeader('Welcome to the Kababatory!'));
+    mainDiv.appendChild(view.createHeroImage('images/fire.jpg'));
+    mainDiv.appendChild(view.createSecondHeader('Our Food'));
+    mainDiv.appendChild(view.createText('We offer the most premium of halal beef, perfectly prepared into the most succulent of meat cutlets before getting smoked for hours on end in our very own charcoal grill.'));
+    mainDiv.appendChild(view.createNavBtn(true, 'to-menu'));
   },
+  createMenuPage: function () {
+    var mainDiv = document.querySelector('div');
+    mainDiv.innerHTML = '';
+    mainDiv.appendChild(view.createHeader('Mouthwatering Meat Treats'));
+    mainDiv.appendChild(view.createHeroImage('images/kabab.jpeg'));
+    mainDiv.appendChild(view.createSecondHeader("Kababatory's Infamous Menu"));
+    mainDiv.appendChild(view.createText('Stay tuned for the menu or give us a call at 1-800-KABAB to find out about the freshest daily catch.'));
+    mainDiv.appendChild(view.createNavBtn(true, 'to-about'));
+    mainDiv.appendChild(view.createNavBtn(false, 'to-home'));
+  },
+  createAboutPage: function () {
+    var mainDiv = document.querySelector('div');
+    mainDiv.innerHTML = '';
+    mainDiv.appendChild(view.createHeader('Kababatory Origins'));
+    mainDiv.appendChild(view.createHeroImage('images/prince.jpg'));
+    mainDiv.appendChild(view.createSecondHeader("A Prince's Journey"));
+    mainDiv.appendChild(view.createText("The story of the prince is one of persistence, hard work and real dedication to one's craft. Starting off on a cart, he slowly built his empire through his infamous liver sandwiches and quickly became a staple in street food, subsequently expanding into a full-blown restaurant where he can fulfill his passion of serving irresistible meats."));
+    mainDiv.appendChild(view.createNavBtn(false, 'to-menu'));
+  }
+};
+var handlers = {
   setUpEvents: function () {
     var mainDiv = document.querySelector('div');
-    var p = document.querySelector('p').innerHTML;
     mainDiv.addEventListener('click', function (event) {
       var elementClicked = event.target;
-      if (elementClicked.className === 'fa fa-arrow-right fa-lg fa-2x' && p.indexOf('halal') !== -1) {
-        handlers.createMenuPage();
-      } else if (elementClicked.className === 'fa fa-arrow-left fa-lg fa-2x' || p.indexOf('call') !== -1) {
+      if (elementClicked.id === 'to-menu') {
+        view.createMenuPage();
+      } else if (elementClicked.id === 'to-home') {
         view.createHomePage();
-      } else if (p.indexOf('call') !== -1 && elementClicked.className === 'fa fa-arrow-right') {
-        handlers.createAboutPage();
+      } else if (elementClicked.id === 'to-about') {
+        view.createAboutPage();
       }
     });
   }
 };
-model.createMainDiv();
+view.createMainDiv();
 view.createHomePage();
-view.setUpEvents();
+handlers.setUpEvents();
